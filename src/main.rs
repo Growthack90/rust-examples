401d48e06c4207f4f7ad88334dcca1222e14691f
Growthack90 <hachmaker90@outlook.it>
2022-04-27 15:18:12 +0200

    add new struct

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,12 @@ struct Color {
     blue: u8,
 }
 
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() {
     for city in [
         City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
@@ -72,19 +78,7 @@ fn main() {
     println!("{number:0>width$}", number=2, width=8);
 
     println!("********************************");
-}
 
-/************************************************/
-/*****************DEBUG STRUCTURE****************/
-/************************************************/
-
-/* #[derive(Debug)]
-struct Structure(i32);
-
-#[derive(Debug)]
-struct Deep(Structure);
-
-fn main() {
     println!("{:?} months in a year.", 12);
     println!("{1:?} {0:?} is the {actor:?} name.",
              "Slater",
@@ -93,4 +87,6 @@ fn main() {
 
     println!("Now {:?} will print!", Structure(3));
     println!("Now {:?} will print!", Deep(Structure(7)));
-} */
+
+    println!("********************************");
+}
